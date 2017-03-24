c2e95eebdc3f932cdc7db8dae1b867e1ba65b18b
Manish Giri <manish.giri.me@gmail.com>
2017-03-23 23:06:56 -0400

    Commit till computeAverageLengthOfWords

--- Module-1/areBothOdd.js
+++ Module-1/areBothOdd.js
@@ -4,17 +4,22 @@ function isLessThan30(num) {
 }
 
 function isEitherEven(num1, num2) {
-  // your code here
-  return (num1 % 2 === 0 || num2 % 2 === 0);
+    // your code here
+    return (num1 % 2 === 0 || num2 % 2 === 0);
 }
 
 function isOddLength(word) {
-  // your code here
-  return word.length % 2 !== 0;
+    // your code here
+    return word.length % 2 !== 0;
 }
 
 function isEvenLength(word) {
-  // your code here
-  return word.length % 2 === 0;
+    // your code here
+    return word.length % 2 === 0;
 }
 
+function isEvenAndGreaterThanTen(num) {
+    // your code here
+
+    return num > 10 && num % 2 === 0;
+}
---
+++ Module-1/computeAverageLengthOfWords.js
@@ -0,0 +1,5 @@
+function computeAverageLengthOfWords(word1, word2) {
+    // your code here
+
+    return (word1.length + word2.length) / 2;
+}
