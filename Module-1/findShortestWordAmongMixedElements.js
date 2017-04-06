67d8b5890e6d3fa76442536a13285befb216e0a8
Manish Giri <manish.giri.me@gmail.com>
2017-04-05 21:35:43 -0400

    commit till getLargestNumberAmongMixedElements()

--- Module-1/findShortestWordAmongMixedElements.js
+++ Module-1/findShortestWordAmongMixedElements.js
@@ -55,3 +55,59 @@ function findSmallestNumberAmongMixedElements(arr) {
   var numbers = arr.filter(e => typeof e === "number");
   return Math.min(...numbers);
 }
+
+
+// Write a function called "getLongestWordOfMixedElements".
+
+// Given an array of mixed types, "getLongestWordOfMixedElements" returns the longest string in the given array.
+
+// Notes:
+// * If the array is empty, it should return an empty string (""). 
+// * If the array contains no strings; it should return an empty string.
+
+function getLongestWordOfMixedElements(arr) {
+  // your code here
+   function hasStrings(arr) {
+    return arr.some(function(e){
+      return typeof e === "string";
+    });
+  }
+  
+  if(!arr.length || !hasStrings(arr)) {
+    return "";
+  }
+  
+  var strings = arr.filter(e => typeof e === "string");
+  var stringLengths = strings.map(e => e.length);
+  
+  var longestStringLength = Math.max(...stringLengths);
+  var longestStringPos = stringLengths.indexOf(longestStringLength);
+  return strings[longestStringPos];
+  
+}
+
+
+// Write a function called "getLargestNumberAmongMixedElements". 
+
+// Given any array, "getLargestNumberAmongMixedElements" returns the largest number in the given array.
+
+// Notes:
+// * The array might contain values of a type other than numbers.
+// * If the array is empty, it should return 0.
+// * If the array contains no numbers, it should return 0.
+
+function getLargestNumberAmongMixedElements(arr) { 
+  // your code here
+  function hasNumbers(arr) {
+    return arr.some(function(e){
+      return typeof e === "number";
+    });
+  }
+  
+  if(!arr.length || !hasNumbers(arr)) {
+    return 0;
+  }
+  var numbers = arr.filter(e => typeof e === "number");
+  return Math.max(...numbers);
+}
+
