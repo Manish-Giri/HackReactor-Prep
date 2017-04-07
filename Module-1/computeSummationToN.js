4011389dde37b5c83af39bb0706ce5efd6ea6d1e
MANISH-GIRI <manish.giri.me@gmail.com>
2017-04-07 16:44:06 -0400

    Commit convertScoreToGradeWithPlusAndMinus()

--- Module-1/computeSummationToN.js
+++ Module-1/computeSummationToN.js
@@ -59,3 +59,48 @@ function convertScoreToGrade(score) {
 // * If the score is between the 8 and 9 (inclusive) of a given range, return the letter with a '+'
 // * There are is no F+ and there is no F-.
 
+function convertScoreToGradeWithPlusAndMinus(score) {
+  // your code here
+    if(score >= 90 && score <= 100) {
+      if(score >= 98 && score <= 100) {
+        return "A+"
+      }
+      else if(score >= 90 && score <= 92) {
+        return "A-"
+      }
+    return "A";
+  }
+  else if(score >= 80 && score <= 89) {
+      if(score >= 88 && score <= 89) {
+        return "B+"
+      }
+      else if(score >= 80 && score <= 82) {
+        return "B-"
+      }
+    return "B";
+  }
+  else if(score >= 70 && score <= 79) {
+    if(score >= 78 && score <= 79) {
+        return "C+"
+      }
+      else if(score >= 70 && score <= 72) {
+        return "C-"
+      }
+    return "C";
+  }
+  else if(score >= 60 && score <= 69) {
+    if(score >= 68 && score <= 69) {
+        return "D+"
+      }
+      else if(score >= 60 && score <= 62) {
+        return "D-"
+      }
+    return "D";
+  }
+  else if(score >= 0 && score <= 59) {
+    return "F";
+  }
+  else {
+    return "INVALID SCORE";
+  }
+}
