6ace160cebe209a8f93be56d984a7a9af784e85d
MANISH-GIRI <manish.giri.me@gmail.com>
2017-04-10 20:51:00 -0400

    Commit multiply

--- Module-1/modulo.js
+++ Module-1/modulo.js
@@ -20,3 +20,17 @@ function modulo(num1, num2) {
   return remainderIsPositive ? num1 : 0 - num1;
 
 }
+
+function multiply(num1, num2) {
+  // your code here
+  if(!num1 || !num2) {
+    return 0;
+  }
+  var original = num1;
+  var times = Math.abs(num2);
+  for(var i = 1; i < times; i++) {
+    num1 += original;
+  }
+  
+  return num2 > 0 ? num1 : -num1;
+}
